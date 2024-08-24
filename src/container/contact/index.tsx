ff582250f37396dd5f53eb9aa6d0d63cbd9077a0
kevinmsavsani <kevinmsavsani@gmail.com>
2024-08-24 20:15:42 +0530

    added contact

--- src/container/contact/index.tsx
+++ src/container/contact/index.tsx
@@ -3,7 +3,7 @@ import { useIntl } from "react-intl";
 const Contact = () => {
   const intl = useIntl();
   return (
-    <div className="border border-neutral-900 pb-4">
+    <div className="border border-neutral-900 pb-16">
       <h1 className="py-10 text-center text-4xl">
         Get in Touch
       </h1>

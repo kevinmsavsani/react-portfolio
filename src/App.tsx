e9275cc31443ba1d284d66daf49729b49e6de7aa
kevinmsavsani <kevinmsavsani@gmail.com>
2024-08-24 18:56:58 +0530

    added technologies

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ProfileNavbar from "./components/navbar/ProfileNavbar";
 import Home from "./container/home";
 import About from "./container/about";
+import Technologies from "./container/technologies";
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
         <ProfileNavbar />
         <Home />
         <About />
+        <Technologies />
       </div>
     </div>
   );

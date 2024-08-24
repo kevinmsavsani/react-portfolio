4d76a7cb7cb87b5457d5aa8cca0041f1556cce1f
kevinmsavsani <kevinmsavsani@gmail.com>
2024-08-24 22:00:25 +0530

    added framer motion

--- src/container/home/index.tsx
+++ src/container/home/index.tsx
@@ -1,7 +1,14 @@
 import { useIntl } from "react-intl";
+import { motion } from "framer-motion";
 import profile from "../../assets/profile.jpg";
+
 const Home = () => {
   const intl = useIntl();
+
+  const container = (delay) => ({
+    hidden: { x: -100, opacity: 0 },
+    visible: { x: 0, opacity: 1, transition: { duration: 0.5, delay: delay } },
+  });
 
   const homeContent = intl.formatMessage({ id: "home-content" });
   return (
@@ -9,20 +16,42 @@ const Home = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+            >
               Kevin Savsani
-            </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
+            >
               Full Stack Developer
-            </span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-tighter"
+            >
               {homeContent}
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img className="rounded-2xl scale-75 opacity-50" src={profile} alt="Kevin Savsani" />
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.2 }}
+              className="rounded-2xl scale-75 opacity-50"
+              src={profile}
+              alt="Kevin Savsani"
+            />
           </div>
         </div>
       </div>

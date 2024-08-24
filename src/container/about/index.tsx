ca5d906e118f26c3fa0a1f92e59994b41ef9df71
kevinmsavsani <kevinmsavsani@gmail.com>
2024-08-24 22:15:12 +0530

    added framer motion

--- src/container/about/index.tsx
+++ src/container/about/index.tsx
@@ -1,5 +1,7 @@
 import { useIntl } from "react-intl";
+import { motion } from "framer-motion";
 import about from "../../assets/about.png";
+
 const About = () => {
   const intl = useIntl();
 
@@ -10,7 +12,12 @@ const About = () => {
         About<span className="text-neutral-500"> Me</span>
       </h1>
       <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2 lg:p-8">
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:p-8"
+        >
           <div className="flex items-center justify-center">
             <img
               className="rounded-2xl scale-75 opacity-50"
@@ -18,14 +25,19 @@ const About = () => {
               alt="Kevin Savsani"
             />
           </div>
-        </div>
-        <div className="w-full lg:w-1/2">
+        </motion.div>
+        <motion.div
+          initial={{ x: 100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2"
+        >
           <div className="flex justify-center lg:justify-start">
             <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
               {aboutContent}
             </p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
